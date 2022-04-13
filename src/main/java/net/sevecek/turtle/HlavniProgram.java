63359a4c97234d333ebc67c59c40de87ef599760
TheJana125 <jtheierova@gmail.com>
2022-04-13 19:33:30 +0200

    jeden domeček

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -8,6 +8,22 @@ public class HlavniProgram {
         Turtle zofka;
 
         zofka = new Turtle();
+        for (int i = 0; i < 4; i++) {
+            zofka.move(150);
+            zofka.turnRight(90);
+        }
+        zofka.penUp();
+        zofka.move(150);
+        zofka.penDown();
+
+        for (int i = 0; i < 2; i++) {
+            zofka.turnRight(60);
+            zofka.move(90);
+        }
+        zofka.penUp();
+        zofka.move(100);
+
+
     }
 
 }
